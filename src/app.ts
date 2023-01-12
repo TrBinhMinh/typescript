5a40a84333d10073058f76262a6e4b27b192f560
LDAP\tbminh <tbminh@cmcglobal.vn>
2023-01-12 19:13:56 +0700

    update to lesson 108

--- src/app.ts
+++ src/app.ts
@@ -1,82 +1,55 @@
-// const names: Array<string | number> = [];
-
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("This is done!");
-//   }, 2000);
-// });
-
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-const mergedObj = merge({ name: "Minh" }, { age: 26 });
-console.log(mergedObj);
-
-interface Lengthy {
-  length: number;
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    console.log("Rendering template");
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
 }
 
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = "Got no value.";
-  if (element.length === 1) {
-    descriptionText = "Got 1 element.";
-  } else if (element.length > 1) {
-    descriptionText = "Got " + element.length + " elements";
-  }
-  return [element, descriptionText];
-}
+@Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+  name = "Minh";
 
-console.log(countAndDescribe(["Wtf"]));
-
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return "Value: " + obj[key];
-}
-
-// console.log(extractAndConvert({ name: "Minh" }, "name"));
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    if (this.data.indexOf(item) === -1) return;
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getItems() {
-    return [...this.data];
+  constructor() {
+    console.log("Creating person object...");
   }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem("Minh");
-textStorage.addItem("Vu");
-textStorage.removeItem("Vu");
-console.log(textStorage.getItems());
+const pers = new Person();
 
-interface CourseGoal {
+console.log(pers);
+
+function Log() {}
+class Product {
   title: string;
-  description: string;
-  completeUntil: Date;
-}
+  private _price: number;
 
-function createCourseGoal(
-  title: string,
-  description: string,
-  date: Date
-): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
-}
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
 
-const names: Readonly<string[]> = ["Minh", "Vu"];
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
