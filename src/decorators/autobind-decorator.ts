ac19480f6223740d11da00f3dfddb84dc252b481
TrBinhMinh <bminh9900@gmail.com>
2023-01-29 21:39:04 +0700

    update to lesson 143

---
+++ src/decorators/autobind-decorator.ts
@@ -0,0 +1,14 @@
+namespace App {
+  export function autobind(_: any, __: string, descriptor: PropertyDescriptor) {
+    // descriptor.value = ProjectInput;
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
