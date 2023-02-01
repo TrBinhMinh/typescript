3c75c95e554fd3826b7d82bf7036090349aa86aa
LDAP\tbminh <tbminh@cmcglobal.vn>
2023-02-01 15:59:23 +0700

    update to lesson 158

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Draggable } from "../models/drag-drop.js";
-import Cmp from "./base-component.js";
-import { Project } from "../models/project.js";
-import { autobind } from "../decorators/autobind-decorator.js";
+import { Draggable } from "../models/drag-drop";
+import Cmp from "./base-component";
+import { Project } from "../models/project";
+import { autobind } from "../decorators/autobind-decorator";
 
 export class ProjectItem
   extends Cmp<HTMLUListElement, HTMLLIElement>
--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import Component from "./base-component.js";
-import { DragTarget } from "../models/drag-drop.js";
-import { autobind } from "../decorators/autobind-decorator.js";
-import { ProjectStatus, Project } from "../models/project.js";
-import { projectState } from "../state/project-state.js";
-import { ProjectItem } from "./project-item.js";
+import Component from "./base-component";
+import { DragTarget } from "../models/drag-drop";
+import { autobind } from "../decorators/autobind-decorator";
+import { ProjectStatus, Project } from "../models/project";
+import { projectState } from "../state/project-state";
+import { ProjectItem } from "./project-item";
 
 export class ProjectList
   extends Component<HTMLDivElement, HTMLElement>
