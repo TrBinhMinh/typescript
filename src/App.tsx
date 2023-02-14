66b6757c08f74f1f449906742b4b703e6bf4b6c4
LDAP\tbminh <tbminh@cmcglobal.vn>
2023-02-14 15:44:50 +0700

    update to lesson 185

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+import { Todo } from "./todo.model";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList onDeleteTodo={todoDeleteHandler} items={todos} />
+    </div>
+  );
+};
 
 export default App;
